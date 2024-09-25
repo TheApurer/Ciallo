c5247d143ae251189c9b6b67953133ea47a0dd3b
景年 <123843248+TheApurer@users.noreply.github.com>
2024-09-25 09:25:44 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from data import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    data = SourceData()
+    return render_template('index.html', form=data, title=data.title)
+
+
+@app.route('/1')
+def corp():
+    data = CorpData()
+    return render_template('index.html', form=data, title=data.title)
+
+
+@app.route('/2')
+def job():
+    data = JobData()
+    return render_template('index.html', form=data, title=data.title)
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=False)
